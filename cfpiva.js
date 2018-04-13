f4b01048e5f9661e50357098410d8bf7ebb728d8
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-04-13 10:18:37 +0200

    using single quotes instead of double quotes

--- cfpiva.js
+++ cfpiva.js
@@ -17,15 +17,15 @@ cfpiva.controllaCF = function (codFisc, callback) {
     codFisc = codFisc.toUpperCase();
     if (codFisc.length != 16)
         return formatReturn(false, 'La lunghezza del codice fiscale non è corretta: il codice fiscale dovrebbe essere lungo esattamente 16 caratteri.', callback);
-    validi = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    validi = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     for (i = 0; i < 16; i++) {
         if (validi.indexOf(codFisc.charAt(i)) == -1)
             return formatReturn(false, 'Il codice fiscale contiene un carattere non valido \'' + codFisc.charAt(i) + '\'. I caratteri validi sono lettere e cifre.', callback);
     }
-    set1 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    set2 = "ABCDEFGHIJABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    setpari = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    setdisp = "BAKPLCQDREVOSFTGUHMINJWZYX";
+    set1 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    set2 = 'ABCDEFGHIJABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    setpari = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    setdisp = 'BAKPLCQDREVOSFTGUHMINJWZYX';
     s = 0;
     for (i = 1; i <= 13; i += 2)
         s += setpari.indexOf(set2.charAt(set1.indexOf(codFisc.charAt(i))));
@@ -44,7 +44,7 @@ cfpiva.controllaPIVA = function (piva, callback) {
     if (piva == '') return '';
     if (piva.length != 11)
         return formatReturn(false, 'La lunghezza della partita IVA non è corretta: la partita IVA dovrebbe essere lunga esattamente 11 caratteri.', callback);
-    var validi = "0123456789";
+    var validi = '0123456789';
     for (i = 0; i < 11; i++) {
         if (validi.indexOf(piva.charAt(i)) == -1)
             return formatReturn(false, 'La partita IVA contiene un carattere non valido \'' + piva.charAt(i) + '\'. I caratteri validi sono solo cifre.', callback);
